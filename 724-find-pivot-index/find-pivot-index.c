2fe6a25bfb99365c50d7627506d558fb180c7cfc
sharmasaurav3821-hub <sharmasaurav3821@gmail.com>
2026-06-06 13:37:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (66.77%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.c
@@ -0,0 +1,17 @@
+ int pivotIndex(int* nums, int numsSize) {
+    int totalSum = 0;
+    for (int i = 0; i < numsSize; i++) {
+        totalSum += nums[i];
+    }
+    
+    int leftSum = 0;
+    for (int i = 0; i < numsSize; i++) {
+        int rightSum = totalSum - leftSum - nums[i];
+        if (leftSum == rightSum) {
+            return i; // Found pivot index
+        }
+        leftSum += nums[i];
+    }
+    
+    return -1; // No pivot index found
+}
